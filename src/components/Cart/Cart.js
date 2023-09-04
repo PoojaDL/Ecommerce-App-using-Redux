573b82c9249609f38df16a3d8b0089f843b00a76
PoojaDL <poojadl2002@gmail.com>
2023-09-04 16:42:43 +0530

    fetch data to backend

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,31 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import Card from "../UI/Card";
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
 import { cartActions } from "../../Store/cart-reducer";
+import { useEffect, useState } from "react";
 
 const Cart = (props) => {
+  const [cartItems, setCartItems] = useState([]);
+
   const dispatch = useDispatch();
   const closeCartHandler = () => {
     dispatch(cartActions.closeCart());
   };
 
-  const cartItems = useSelector((state) => state.cartFunction.items);
+  // const cartItems = useSelector((state) => state.cartFunction.items);
+
+  useEffect(() => {
+    fetch("https://movie-app-34d2c-default-rtdb.firebaseio.com/cart.json")
+      .then((data) => {
+        if (data.ok) {
+          return data.json();
+        } else {
+          throw new Error("Error");
+        }
+      })
+      .then((res) => setCartItems(res.items));
+  }, []);
 
   return (
     <Card className={classes.cart}>
